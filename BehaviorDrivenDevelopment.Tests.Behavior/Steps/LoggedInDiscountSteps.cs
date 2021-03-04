1b5da58c759aa7f54dbcee51f2d2ac95f77156c6
lucas.cruz <lucas.cruz@ceracare.co.uk>
2021-03-04 14:12:27 +0000

    Add project files

---
+++ BehaviorDrivenDevelopment.Tests.Behavior/Steps/LoggedInDiscountSteps.cs
@@ -0,0 +1,61 @@
+﻿using BehaviorDriverDevelopment.Service;
+using FluentAssertions;
+using TechTalk.SpecFlow;
+
+namespace BehaviorDrivenDevelopment.Tests.Behavior.Steps
+{
+    [Binding]
+    public class LoggedInDiscountSteps
+    {
+        private User _user;
+        private Basket _basket;
+        private readonly IPricingService _pricingService = new PricingService();
+        
+        [Given(@"a user that is not logged in")]
+        public void GivenAUserThatIsNotLoggedIn()
+        {
+            _user = new User
+            {
+                IsLoggedIn = false
+            };
+        }
+
+        [Given(@"an empty basket")]
+        public void GivenAnEmptyBasket()
+        {
+            _basket = new Basket
+            {
+                User = _user
+            };
+        }
+        
+        [Given(@"a user that is logged in")]
+        public void GivenAUserThatIsLoggedIn()
+        {
+            _user = new User
+            {
+                IsLoggedIn = true
+            };
+        }
+    
+        [When(@"a (.*) that costs (.*) GBP is added to the basket")]
+        public void WhenATshirtThatCosts5GbpIsAddedToTheBasket(
+            string itemName, 
+            decimal price)
+        {
+            _basket.Products.Add(new Product
+            {
+                Name = itemName,
+                Price = price
+            });
+        }
+
+        [Then(@"the basket value is (.*) GBP")]
+        public void ThenTheBasketValueIs5Gbp(
+            decimal expectedBasketValue)
+        {
+            var basketValue = _pricingService.GetBasketTotalAmount(_basket);
+            basketValue.Should().Be(expectedBasketValue);
+        }
+    }
+}
---
+++ BehaviorDriverDevelopment.Service/PricingService.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BehaviorDriverDevelopment.Service
+{
+    public class PricingService : IPricingService
+    {
+        public decimal GetBasketTotalAmount(Basket basket)
+        {
+            if (basket == null)
+            {
+                throw new ArgumentNullException(nameof(basket));
+            }
+
+            decimal basketValue = 0;
+    
+            if (basket.Products.Any(b => b.Name == "t-shirt"))
+            {
+                basketValue = 5;
+            }
+            
+            if (basket.Products.Any(b => b.Name == "dress"))
+            {
+                basketValue = 100;
+            }
+
+            if (basket.User.IsLoggedIn)
+            {
+                return basketValue - (basketValue * 0.05m);
+            }
+
+            return basketValue;
+        }
+    }
+
+    public interface IPricingService
+    {
+        decimal GetBasketTotalAmount(Basket basket);
+    }
+
+    public class User
+    {
+        public bool IsLoggedIn { get; init; }
+    }
+
+    public class Basket
+    {
+        public User User { get; init; }
+        public List<Product> Products { get; } = new();
+    }
+
+    public class Product
+    {
+        public string Name { get; init; }
+        public decimal Price { get; init; }
+    }
+}
